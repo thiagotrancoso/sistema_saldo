c3f770af0357120fdd8aa1a2f85707a9ced151b9
Thiago Trancoso <thiagostrn@gmail.com>
2022-06-07 17:31:56 -0300

    Organização das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SiteController@home')->name('site.home');
 
 Auth::routes();
 
-Route::get('/admin', 'AdminController@dashboard')->name('admin.dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', 'AdminController@dashboard')->name('admin.dashboard');
+});
