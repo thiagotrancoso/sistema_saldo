aa8f7cbb54fd48771c94832bb8fda654a7d64bc6
Thiago Trancoso <thiagostrn@gmail.com>
2022-06-08 10:26:11 -0300

    Adicionado usuário na seed de users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,5 +17,11 @@ class UsersTableSeeder extends Seeder
             'email'    => 'thiagostrn@gmail.com',
             'password' => bcrypt('12345678')
         ]);
+
+        User::create([
+            'name'     => 'Fulano',
+            'email'    => 'fulano@mail.com',
+            'password' => bcrypt('12345678')
+        ]);
     }
 }
