818356a78c94838cc534528c72981d41bc019ae9
Thiago Trancoso <thiagostrn@gmail.com>
2022-06-09 21:49:20 -0300

    Desabilitar register

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -80,15 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Sistema simples de saldo
                 </div>
             </div>
         </div>
